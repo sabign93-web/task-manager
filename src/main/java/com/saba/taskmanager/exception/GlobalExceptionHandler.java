f3e8d9668a848b0c78bcd4b315bf12e55cf71705
Saba <sabign93@gmail.com>
2026-05-15 18:32:31 +0300

    update unused items

--- src/main/java/com/saba/taskmanager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/saba/taskmanager/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.saba.taskmanager.exception;
 
-import com.saba.taskmanager.entity.Company;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
